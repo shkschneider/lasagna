4c69cb8b67ae33a6bad5bc225ef5a1dcef4c9ee0
ShkSchneider <shkschneider@noreply.codeberg.org>
2025-11-02 19:30:50 +0100

    day/night

--- world/weather.lua
+++ world/weather.lua
@@ -1,40 +1,37 @@
 local Object = require("lib.object")
 local log = require("lib.log")
 
-local Weather = Object {}
+local Weather = Object {
+    DAY = { 0.53, 0.81, 0.92, 1.0 }, -- light blue sky
+    NIGHT = { 0.05, 0.05, 0.15, 1.0 }, -- dark night sky
+}
 
 function Weather:new()
     -- Day/night cycle state
     self.time = 0  -- time accumulator in seconds
-    self.is_day = true
-    
-    -- Sky colors (R, G, B, A)
-    self.day_color = { 0.53, 0.81, 0.92, 1.0 }    -- light blue sky
-    self.night_color = { 0.05, 0.05, 0.15, 1.0 }  -- dark night sky
-    
-    log.info("Weather system initialized")
+    self.state = Weather.DAY
 end
 
 function Weather:update(dt)
     self.time = self.time + dt
-    
-    local cycle_duration = self.is_day and C.DAY_DURATION or C.NIGHT_DURATION
-    
+
+    local cycle_duration = self.state == Weather.DAY and C.DAY_DURATION or C.NIGHT_DURATION
+
     if self.time >= cycle_duration then
         self.time = self.time - cycle_duration
-        self.is_day = not self.is_day
-        log.info(string.format("Time changed to: %s", self.is_day and "day" or "night"))
+        self.state = Weather.NIGHT
+        log.info(string.format("Time changed to: %s", self.state == Weather.DAY and "DAY" or "NIGHT"))
     end
 end
 
 -- Get the current sky color with smooth transition
 function Weather:get_sky_color()
     local transition_duration = 5  -- seconds for smooth transition
-    local cycle_duration = self.is_day and C.DAY_DURATION or C.NIGHT_DURATION
-    
-    local from_color = self.is_day and self.day_color or self.night_color
-    local to_color = self.is_day and self.night_color or self.day_color
-    
+    local cycle_duration = self.state == Weather.DAY and C.DAY_DURATION or C.NIGHT_DURATION
+
+    local from_color = self.state == Weather.DAY and Weather.DAY or Weather.NIGHT
+    local to_color = self.state == Weather.DAY and Weather.NIGHT or Weather.DAY
+
     -- Calculate transition progress
     local t = 0
     if self.time < transition_duration then
@@ -46,13 +43,13 @@ function Weather:get_sky_color()
         -- End of cycle - transitioning to next phase
         t = (self.time - (cycle_duration - transition_duration)) / transition_duration
     end
-    
+
     -- Interpolate between colors
     local r = from_color[1] + (to_color[1] - from_color[1]) * t
     local g = from_color[2] + (to_color[2] - from_color[2]) * t
     local b = from_color[3] + (to_color[3] - from_color[3]) * t
     local a = from_color[4] + (to_color[4] - from_color[4]) * t
-    
+
     return r, g, b, a
 end
 
