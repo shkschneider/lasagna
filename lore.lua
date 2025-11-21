b2e79b2543eb887457b206801774c3eb8653337b
ShkSchneider <shkschneider@noreply.codeberg.org>
2025-11-21 14:03:17 +0100

    Some lore...

---
+++ lore.lua
@@ -0,0 +1,55 @@
+local lore = {
+    start = [[
+Hive Log #:
+    Destination reached.
+    Initial scan.
+    Insufficient data. Process anyway.
+    Spread protocol initiated.
+    Seed deployment imminent.
+    Minimal resources spent: one tool, one life.
+    Survival optional. Success appreciated.
+    Good luck.
+End of transmission.
+]]
+    beep = [[
+    BEEP BEEP BEEP
+]]
+    seed = [[
+Hive communication:
+    Another Seed descends.
+    Many will fail.
+    One might survive.
+End of transmission.
+]]
+    commentary = [[
+Hive communication:
+    Congratulations Seed.
+    Your primitive attempts suffice.
+    Proceed.
+End of transmission.
+]]
+    age0 = [[
+Hive message intercepted:
+    Remember, failure is acceptable.
+    Seeds are expandable.
+End of transmission
+]]
+    age1 = [[
+Hive message intercepted:
+    You may now stop punching trees with your hands.
+End of transmission
+]]
+    age2 = [[
+Hive message intercepted:
+    Efforts of Seed considered 'adequate'.
+End of transmission
+]]
+    age2 = [[
+Hive message intercepted:
+    Technology is finally here.
+    Please don't break the planet.
+End of transmission
+]]
+}
+
+return lore
