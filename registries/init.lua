dc81e0cc4289ca7017a8f826d71b4930a512f2ad
ShkSchneider <shkschneider@noreply.codeberg.org>
2025-11-22 13:50:02 +0100

    registries...

---
+++ registries/init.lua
@@ -0,0 +1,10 @@
+local BlocksRegistry = require("registries/blocks")
+local ItemsRegistry = require("registries/items")
+
+local Registry = {}
+
+function Registry.block() end
+
+function Registry.item() end
+
+return Registry
