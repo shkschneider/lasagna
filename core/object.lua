d49ff437a2db87995305645ac55451f1097820b2
ShkSchneider <shkschneider@noreply.codeberg.org>
2025-11-26 10:19:55 +0100

    core/object: minified

--- core/object.lua
+++ core/object.lua
@@ -1,25 +1,9 @@
-require "core"
-
 local Love = require "core.love"
 
 return function(...)
-    local object = {
-        load = Love.load,
-        update = Love.update,
-        draw = Love.draw,
-        keypressed = Love.keypressed,
-        keyreleased = Love.keyreleased,
-        mousepressed = Love.mousepressed,
-        mousereleased = Love.mousereleased,
-        wheelmoved = Love.wheelmoved,
-        textinput = Love.textinput,
-        resize = Love.resize,
-        focus = Love.focus,
-        quit = Love.quit,
-        tostring = Love.tostring,
-    }
+    local object = {}
     for key, value in pairs(...) do
         object[key] = value
     end
-    return object
+    return setmetatable(object, { __index = Love })
 end
